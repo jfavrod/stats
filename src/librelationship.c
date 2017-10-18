6f6675fa74bc2b5f84783a35900cf625400acdd7
not--p <mail@jasonfavrod.com>
2017-10-17 16:29:45 -1000

    Started on librelationship

---
+++ src/librelationship.c
@@ -0,0 +1,8 @@
+#include "librelationship.h"
+
+
+double r_coeff(double X[], double Y[])
+{
+    return;
+}
+
---
+++ src/librelationship.h
@@ -0,0 +1,10 @@
+#ifndef LIBRELATIONSHIP_H
+#define LIBRELATIONSHIP_H
+
+#include "libcentral.h"
+#include "libvariance.h"
+
+double r_coeff(double X[], double Y[]);
+
+#endif
+
